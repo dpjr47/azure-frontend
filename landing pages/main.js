8016300b410bc805a4687e5a880f84293b6e2239
mhdaslmnzr <aslam99955@gmail.com>
2021-07-02 14:28:04 +0530

    second

---
+++ landing pages/main.js
@@ -0,0 +1,27 @@
+function mainshow1(){
+    document.getElementById("faq").style.display="inline";
+    document.getElementById("home").style.display="none";
+    document.getElementById("test").style.display="none";
+    document.getElementById("account").style.display="none";
+}
+
+function mainshow2(){
+    document.getElementById("test").style.display="inline";
+    document.getElementById("home").style.display="none";
+    document.getElementById("faq").style.display="none";
+    document.getElementById("account").style.display="none";
+}
+
+function mainshow3(){
+    document.getElementById("home").style.display="inline";
+    document.getElementById("faq").style.display="none";
+    document.getElementById("test").style.display="none";
+    document.getElementById("account").style.display="none";
+}
+
+function mainshow4(){
+    document.getElementById("account").style.display="inline";
+    document.getElementById("home").style.display="none";
+    document.getElementById("test").style.display="none";
+    document.getElementById("faq").style.display="none";
+}
