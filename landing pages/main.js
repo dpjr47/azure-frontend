0a2ccb7e5bef952a6797cf3b500bd67e3f2d4837
mhdaslmnzr <aslam99955@gmail.com>
2021-07-04 01:42:31 +0530

    news

--- landing pages/main.js
+++ landing pages/main.js
@@ -26,10 +26,32 @@ function mainshow4(){
     document.getElementById("faq").style.display="none";
 }
 
-var loadFile = function(event) {
-    var output = document.getElementById('output');
-    output.src = URL.createObjectURL(event.target.files[0]);
-    output.onload = function() {
-    URL.revokeObjectURL(output.src) // free memory
-    }
-};
+//news api
+
+var url = 'https://newsapi.org/v2/everything?q=covid&apiKey=86a2d82c7f75420e96cb797a13c66dfd'
+var req = new Request(url);
+fetch(req)
+    .then(function(response) {
+       // console.log(response.json());
+      return response.json();
+      }).then(function(jsonResult){
+        console.log(jsonResult)
+        //console.log(jsonResult['articles'])
+
+        var disp1 = document.getElementById('ubox');
+        var disp2 = document.getElementById('ubox2');
+        for (var i=0;i<jsonResult['articles'].length;i++)
+        {
+           console.log(jsonResult['articles'][i]['title']);
+           var divs = document.createElement('li');
+           var hr = document.createElement('hr')
+            divs.innerHTML = jsonResult['articles'][i]['title']+"\n";
+            var kaali_div = document.createElement('div')
+            kaali_div.innerHTML = "\n";
+            disp1.appendChild(divs);
+            disp1.appendChild(kaali_div)
+            disp1.appendChild(hr)
+            
+        }
+
+      })
