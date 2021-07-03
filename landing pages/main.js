57a7dee104c5e5719500497872841aea714b84c7
mhdaslmnzr <aslam99955@gmail.com>
2021-07-04 01:18:22 +0530

    change photo

--- landing pages/main.js
+++ landing pages/main.js
@@ -26,6 +26,10 @@ function mainshow4(){
     document.getElementById("faq").style.display="none";
 }
 
-function profile(){
-    document.getElementById("edit_profile").style.display.display="inline";
-}
+var loadFile = function(event) {
+    var output = document.getElementById('output');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function() {
+    URL.revokeObjectURL(output.src) // free memory
+    }
+};
